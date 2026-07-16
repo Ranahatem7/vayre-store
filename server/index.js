1e21bbe5508f7d1f1ad3c4d1ac3927015b447780
Ranahatem7 <Ranahatem.ab@gmail.com>
2026-07-16 18:10:04 +0300

    server initiated

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Load environment variables from .env
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "Vayre API is running " });
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
